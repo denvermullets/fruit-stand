8ba194946c7865691f1b818a78a426f6a744bcf6
denvermullets <devmullets@gmail.com>
2025-08-07 19:34:57 -0400

    chore(init): mulletstack init

--- server/src/database/db.d.ts
+++
@@ -1,4 +0,0 @@
-import knex from "knex";
-declare const db: knex.Knex<any, unknown[]>;
-export default db;
-//# sourceMappingURL=db.d.ts.map
--- server/src/database/knexfile.d.ts
+++
@@ -1,7 +0,0 @@
-import { Knex } from "knex";
-interface IKnexConfig {
-    [key: string]: Knex.Config;
-}
-declare const configs: IKnexConfig;
-export default configs;
-//# sourceMappingURL=knexfile.d.ts.map
